7cb09e42e4fdf75afe8649c5e3b6661061dc3208
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-03-05 18:36:04 -0800

    Formatting pass

--- main.cpp
+++ main.cpp
@@ -1,31 +1,28 @@
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
-#include <iostream>
 
-std::string read_file(std::ifstream&& file){
-
-    std::stringstream content{};
-    {
-        std::string line;
-        while(std::getline(file, line)) content << line << '\n';
-    }
-    return content.str();
+std::string read_file(std::ifstream && file) {
+	std::stringstream content{};
+	{
+		std::string line;
+		while (std::getline(file, line)) content << line << '\n';
+	}
+	return content.str();
 }
 
-int main(int arg_count, const char** args){
-    
-    std::string filename;
-    {
-        int arg_num = 1;
-        while(args[arg_num] != nullptr){
-        
-            if(arg_num == arg_count - 1) filename = args[arg_num];
-            arg_num++;
-        }
-    }
+int main(int arg_count, const char ** args) {
+	std::string filename;
+	{
+		int arg_num = 1;
+		while (args[arg_num] != nullptr) {
+			if (arg_num == arg_count - 1) filename = args[arg_num];
+			arg_num++;
+		}
+	}
 
-    auto data = read_file(std::ifstream{filename});
+	auto data = read_file(std::ifstream{filename});
 
-    std::cout << data << std::flush;
+	std::cout << data << std::flush;
 }
