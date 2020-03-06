b2603b4f0972e752f21b4450c40dea77aae77a93
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-03-05 18:34:21 -0800

    Finished parsing algorithm

--- grammar.cpp
+++ grammar.cpp
@@ -3,36 +3,102 @@
 #include <algorithm>
 #include <cctype>
 
-grammar grammar::parse_from_file(const std::string& data) {
+grammar grammar::parse_from_file(const std::string & data) {
+	grammar to_ret{};
+	auto	iter	= data.begin();
+	char	nonterm = 0;
 
-    grammar to_ret{};
+	const auto consume_whitespace = [&iter, &data]() {
+		while (iter != data.end() and isspace(*iter) and *iter != '\n') iter++;
+	};
 
-    auto iter = data.begin();
-    while(iter != data.end()){
-        // Remove whitespace
-        while(isspace(*iter) and *iter != '\n') iter++;
+	std::vector<int> rule_list{};
+	while (iter != data.end()) {
+		// Remove initial whitespace
+		consume_whitespace();
 
-        
-    }
+		// Read initial symbol
+		if (isupper(*iter) and to_ret.symbols.count(*iter) == 0) {
+			nonterm = *iter;
+			iter++;
+		}
 
-    return to_ret;
+		// remove whitespace between symbol and hyphen
+		consume_whitespace();
+
+		// consume all hyphens
+		while (iter != data.end() and *iter == '-') { iter++; }
+
+		// remove whitespace
+		consume_whitespace();
+
+		// Go to the end of the line
+		while (iter != data.end() and *iter != '\n') {
+			if (not isspace(*iter)) {
+				if (*iter == rule_sep_char) {
+					rule_list.push_back(rule_sep);
+				} else if (isupper(*iter)) {
+					rule_list.push_back(to_ret.nonterminal(*iter));
+				} else {
+					// lower
+					rule_list.push_back(to_ret.terminal(*iter));
+				}
+			}
+
+			iter++;
+		}
+
+		to_ret.rules.emplace(to_ret.nonterminal(nonterm), std::move(rule_list));
+		rule_list = {};
+		iter++;
+	}
+
+	return to_ret;
 }
 
-std::vector<std::vector<int>> rules(int symbol) const {
+std::vector<std::vector<int>> grammar::rule_matrix(int symbol) const {
+	std::vector<std::vector<int>> to_ret{{}};
 
-    std::vector<std::vector<int>> to_ret{{}};
+	const auto & all_rules = rules.at(symbol);
 
-    const auto & all_rules = rules.at(symbol);
+	to_ret.reserve(std::count(all_rules.begin(), all_rules.end(), 0));
 
-    to_ret.reserve(std::count(all_rules.begin(), all_rules.end(), 0));
+	for (const auto & symbol : all_rules) {
+		if (symbol == rule_sep) {
+			to_ret.emplace_back();
+		} else {
+			to_ret.back().push_back(symbol);
+		}
+	}
 
-    for(const auto & symbol : all_rules ){
-        if(symbol == rule_sep){
-            to_ret.emplace_back();
-        }else{
-            to_ret.back().push_back(symbol);
-        }
-    }
+	return to_ret;
+}
 
-    return to_ret;
+int grammar::nonterminal(char symbol) {
+	auto iter = std::find_if(
+		symbols.begin(), symbols.end(),
+		[&](const auto & item) -> bool { return item.second == symbol; });
+	if (iter != symbols.end()) {
+		return iter->first;
+	} else {
+		const auto to_ret = next_nonterminal;
+		symbols[to_ret]	  = symbol;
+		// Rules are added in the parser / manually
+		next_nonterminal++;
+		return to_ret;
+	}
+}
+
+int grammar::terminal(char symbol) {
+	auto iter = std::find_if(
+		symbols.begin(), symbols.end(),
+		[&](const auto & item) -> bool { return item.second == symbol; });
+	if (iter != symbols.end()) {
+		return iter->first;
+	} else {
+		const auto to_ret = next_terminal;
+		symbols[to_ret]	  = symbol;
+		next_nonterminal--;
+		return to_ret;
+	}
 }
--- grammar.hpp
+++ grammar.hpp
@@ -1,23 +1,36 @@
 #ifndef GRAMMAR_HPP
 #define GRAMMAR_HPP
 
-#include <vector>
+#include <limits>
 #include <map>
+#include <vector>
 
-class grammar{
-    public:
-        static constexpr int rule_sep = 0;
-        static constexpr char rule_sep_char = '|';
+class grammar {
+   public:
+	static constexpr int  rule_sep		= 0;
+	static constexpr char rule_sep_char = '|';
 
-        static grammar parse_from_file(const std::string& data);
+	static constexpr int epsilon = std::numeric_limits<char>::max() + 1;
 
-        // Returns each rule as its own vector
-        std::vector<std::vector<int>> rules(int symbol) const;
+	static grammar parse_from_file(const std::string & data);
 
-        auto nonterm_count() const { return rules.size(); }
-    private:
-        std::map<int, char> symbols{ { rule_sep, rule_sep_char } };
-        std::map<int, std::vector<int>> rules{};
+	// Returns each rule as its own vector
+	std::vector<std::vector<int>> rule_matrix(int symbol) const;
+
+	auto nonterminal_count() const { return rules.size(); }
+	auto terminal_count() const {
+		return symbols.size() - (1 + nonterminal_count());
+	}
+
+	int nonterminal(char symbol);
+
+	int terminal(char symbol);
+
+   private:
+	std::map<int, char>				symbols{{rule_sep, rule_sep_char}};
+	std::map<int, std::vector<int>> rules{};
+	int								next_nonterminal = 1;
+	int								next_terminal	 = -1;
 };
 
 #endif
