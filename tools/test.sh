3e8f66e9f018d3b92fe0a547a70667429aa18d42
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-04-01 04:21:52 -0700

    The test script saves the results of the test and the previous compilation

--- tools/test.sh
+++ tools/test.sh
@@ -3,10 +3,15 @@
 [ -d tools ] || { echo "Could not find tools directory"; exit ; }
 [ -d tests ] || { echo "Could not find tests directory"; exit ; }
 
-tools/build.sh || exit
+tools/build.sh 2> build_log.txt || exit
 
+[ "$(grep -ic error build_log.txt)" -ne 0 ] && echo "There are build errors" && exit
+
+test_log="test_log.txt"
+
+date > "$test_log"
 for file in tests/*; do
-    echo "$file"
-    build/check_grammar "$file"
+    echo "$file" | tee -a "$test_log"
+    build/check_grammar "$file" 2>&1 | tee -a "$test_log"
     echo ""
 done
