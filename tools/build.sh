cc7b647a9a2b5734ba815fcd77300ec9d68f0bc5
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-04-06 01:06:40 -0700

    Removed the formatting part of the build script as it appears to not work

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-for file in src/*.{cpp,hpp}; do
-    echo "Formatting $file"
-    clang-format-9 -i "$file"
-done
 clear
 
 pushd build || { echo "Making build directory" && mkdir build && cd build || exit ; }
